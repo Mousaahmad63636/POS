eb30c50163aa07f7ca9c1ac99622a1471cf07b56
Mr Ahmad <mousaahmad63636@gmail.com>
2025-05-31 14:43:10 +0300

    fix edit sec attempt4

--- QuickTechSystems/Converters/NullToBoolConverter.cs
+++ QuickTechSystems/Converters/NullToBoolConverter.cs
@@ -5,19 +5,53 @@ using System.Windows.Data;
 
 namespace QuickTechSystems.WPF.Converters
 {
+    /// <summary>
+    /// Converts null values to false and non-null values to true
+    /// </summary>
     public class NullToBoolConverter : IValueConverter
     {
-        public static readonly NullToBoolConverter Instance = new NullToBoolConverter();
+        private static NullToBoolConverter _instance;
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        /// <summary>
+        /// Singleton instance for static resource usage
+        /// </summary>
+        public static NullToBoolConverter Instance
         {
-            // Returns true if value is not null, false if it is null
-            return value != null;
+            get
+            {
+                if (_instance == null)
+                    _instance = new NullToBoolConverter();
+                return _instance;
+            }
         }
 
+        /// <summary>
+        /// Converts a value to boolean based on null check
+        /// </summary>
+        /// <param name="value">The value to convert</param>
+        /// <param name="targetType">The target type (not used)</param>
+        /// <param name="parameter">Optional parameter for inversion (set to "Invert" to invert the result)</param>
+        /// <param name="culture">The culture (not used)</param>
+        /// <returns>False if value is null, True if value is not null (or inverted if parameter is "Invert")</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = value != null;
+
+            // Check if we should invert the result
+            if (parameter != null && parameter.ToString().Equals("Invert", StringComparison.OrdinalIgnoreCase))
+            {
+                result = !result;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Not implemented - this converter is one-way only
+        /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException("NullToBoolConverter is a one-way converter only.");
         }
     }
 }
